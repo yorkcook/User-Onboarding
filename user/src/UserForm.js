6d2d756e643c0cdd1cf462f1a5df254bcaf69c7f
York Cook <yorkcook@Johnny-Castle.lan1>
2019-07-17 16:53:09 -0400

    WIP: MVP

--- user/src/UserForm.js
+++ user/src/UserForm.js
@@ -1,34 +1,57 @@
 import React from "react";
 
 import { withFormik, Form, Field } from "formik";
-//import axios from "axios";
+import axios from "axios";
 import * as Yup from "yup";
 
-const UserForm = () => {
+const UserForm = ({ values, errors, touched, isSubmitting }) => {
   return (
     <Form>
       Name:
+      {touched.name && errors.name && <p>{errors.name}</p>}
       <Field type="text" name="name" placeholder="name" />
       Email:
+      {touched.email && errors.email && <p>{errors.email}</p>}
       <Field type="text" name="email" placeholder="email" />
       Password:
+      {touched.password && errors.password && <p>{errors.password}</p>}
       <Field type="text" name="password" placeholder="password" />
       Agree:
       <Field type="checkbox" name="submit" placeholder="Submit" />
-      <button>Submit</button>
+      <button disabled={isSubmitting}>Submit</button>
     </Form>
   );
 };
 
 export default withFormik({
-  mapPropsToValue: () => {
+  mapPropsToValue: ({ email, password }) => {
     return {
       name: "",
-      email: "",
-      password: "",
+      email: email || "",
+      password: password || "",
       submit: false
     };
   },
+
+  handleSubmit(values, { resetForm, setErrors, setSubmitting }) {
+    if (values.email === "alreadytaken@email.com") {
+      setErrors({ email: "Email already taken" });
+    } else {
+      axios
+        .post("https://reqres.in/api/users", values)
+        .then(res => {
+          console.log(res);
+          resetForm();
+          setSubmitting(false);
+          window.alert("Some response here");
+        })
+        .catch(err => {
+          console.log(err);
+          setSubmitting(false);
+        });
+    }
+  },
+
   validationSchema: Yup.object().shape({
     name: Yup.string()
       .min(3, "Name must be at least 3 characters long")
